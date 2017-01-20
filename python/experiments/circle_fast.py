6882ea27696a4b3ed18492faa48f1b235b6a27a8
Exequiel Sepúlveda <exequiel@Exequiels-MacBook-Pro.local>
2017-01-20 22:04:14 +1030

    adding circle_fast.py:

---
+++ python/experiments/circle_fast.py
@@ -0,0 +1,123 @@
+import numpy as np
+import time
+
+#see: https://en.wikipedia.org/wiki/Circumscribed_circle
+
+def center_vectorized(points):
+	points_prima = points - points[0,:,:]
+	points_prima_squared = np.sum(points_prima**2,axis=1)
+	#print "points_prima.shape",points_prima.shape
+	#print "points_prima_squared.shape",points_prima_squared.shape
+
+	Dp = 2*(points_prima[1,0]*points_prima[2,1]-points_prima[1,1]*points_prima[2,0])
+	#print "Dp.shape",Dp.shape
+
+	#print "points_prima_squared",points_prima_squared,"Dp",Dp
+
+	Up =np.empty((2,n))
+
+	Up[0] = (points_prima[2,1]*points_prima_squared[1]-points_prima[1,1]*points_prima_squared[2])/Dp
+	Up[1] = (points_prima[1,0]*points_prima_squared[2]-points_prima[2,0]*points_prima_squared[1])/Dp
+
+	#print Up[:,:5]
+	
+	return Up + points[0,:,:]
+	
+def center_loop(A,B,C):
+	Ap = A-A
+	Bp = B-A
+	Cp = C-A
+
+	B2=np.sum(Bp**2)
+	C2=np.sum(Cp**2)
+
+	#print "B2",B2,"C2",C2
+
+
+	Dp = 2*(Bp[0]*Cp[1]-Bp[1]*Cp[0])
+
+	#print "Dp",Dp
+
+	Up =np.empty(2)
+
+	Up[0] = (Cp[1]*(B2)-Bp[1]*(C2))/Dp
+
+	Up[1] = (Bp[0]*(C2)-Cp[0]*(B2))/Dp
+
+
+	U = Up + A
+	return U
+	
+if __name__ == "__main__":
+
+	n = 100000
+
+	points = np.random.random(size=(3,2,n)) * 100.0
+
+	centers_1 = np.empty((2,n))
+	t1 = time.time()
+
+	for i in range(n):
+		A = points[0,:,i]
+		B = points[1,:,i]
+		C = points[2,:,i]
+		centers_1[:,i] =center_loop(A,B,C)
+
+	t2 = time.time()
+	
+	d1 = t2-t1
+
+	print d1
+
+	t1 = time.time()
+	centers_2 = center_vectorized(points)
+	t2 = time.time()
+	
+	d2 = t2-t1
+	print d2
+	
+	print "improvement",d1/d2
+
+	for i in range(n):
+		assert centers_1[0,i] == centers_2[0,i]
+		assert centers_1[1,i] == centers_2[1,i]
+
+	quit()
+	A = np.array([3.0,4.0])
+	B = np.array([2.0,-1.0])
+	C = np.array([-1.0,5.0])
+
+	t1 = time.time()
+	for i in range(10000):
+
+
+		Ap = A-A
+		Bp = B-A
+		Cp = C-A
+
+		B2=np.sum(Bp**2)
+		C2=np.sum(Cp**2)
+
+		#print "B2",B2,"C2",C2
+
+
+		Dp = 2*(Bp[0]*Cp[1]-Bp[1]*Cp[0])
+
+		#print "Dp",Dp
+
+		Up =np.empty(2)
+
+		Up[0] = (Cp[1]*(B2)-Bp[1]*(C2))/Dp
+
+		Up[1] = (Bp[0]*(C2)-Cp[0]*(B2))/Dp
+
+
+		U = Up + A
+
+		#print U
+
+	t2 = time.time()
+	print t2-t1
+	print Up
+		
+	print 5.0/14.0, 27.0/14.0
--- python/fixer.py
+++ python/fixer.py
@@ -26,8 +26,14 @@ def repair_1(x,y):
                 k += 1
     return k
 
-def repair_2(x,y,debug=False):
-    '''this function reparis a prediction at breaks sections.
+def repair_2(x,y,increasing=True,debug=False):
+    if increasing:
+        return repair_2_increasing(x,y,debug)
+    else:
+        return repair_2_decreasing(x,y,debug)
+
+def repair_2_increasing(x,y,debug=False):
+    '''this function reparis a prediction at breaked sections.
     for example: 5 5 5 5 4 4 4 6 6 
     Under the assumtion tag are increasing,  if there is one that is going down we should fix it.
     Three cases:
@@ -48,6 +54,62 @@ def repair_2(x,y,debug=False):
         #9 11 10
         if debug: print "i_start",i_start
         for i in xrange(i_start+1,n-1):
+            if debug: print "(i-1|i|i+1)",i,x[i],":",y[i-1],y[i],y[i+1]
+            if y[i] < y[i-1]:
+                k += 1
+                if y[i-1] >= y[i+1]: #make it equal to i-1
+                    if debug: print "changing",y[i],"by",y[i-1]
+                    y[i] = y[i-1]
+                else:
+                    if (x[i] - x[i-1]) <= (x[i+1] - x[i]): #closet to left
+                        if debug: print "changing",y[i],"by",y[i-1]
+                        y[i] = y[i-1]
+                    else:
+                        if debug: print "changing",y[i],"by",y[i+1]
+                        y[i] = y[i+1]
+                        
+            elif y[i] > y[i-1]:
+                if y[i-1] == y[i+1]: #neighbors are the same
+                    k += 1
+                    if debug: print "changing",y[i],"by",y[i-1]
+                    y[i] = y[i-1]
+                elif y[i-1] < y[i+1] and y[i] > y[i+1]: #neighbors are increasingly consisten
+                    if (x[i] - x[i-1]) <= (x[i+1] - x[i]): #closet to left
+                        if debug: print "changing",y[i],"by",y[i-1]
+                        y[i] = y[i-1]
+                    else:
+                        if debug: print "changing",y[i],"by",y[i+1]
+                        y[i] = y[i+1]
+
+        #the last one
+        if y[-1] < y[-2]:
+            if debug: print "changing",y[-1],"by",y[-2]
+            y[-1] = y[-2]
+        
+    return k
+
+def repair_2_decreasing(x,y,debug=False):
+    '''this function reparis a prediction at breaked sections.
+    for example: 6 6 6 6 4 4 4 5 5 
+    Under the assumtion tag are decreasing,  if there is one that is going down we should fix it.
+    Three cases:
+    a) to the right there is a value greater than to the left, therefore must be the same value as the left
+    b) to the right there is a value lower than to the left, therefore we choose the closest.
+    c) to the right there is a value equal, the same applied as a)
+    '''
+    n = len(x)
+
+    k = 0
+    if n >=3:
+        #look for the first 6
+        i_start = 2
+        for i in xrange(n-2,2,-1):
+            if y[i] == 6:
+                i_start = i
+                break
+        #9 11 10
+        if debug: print "i_start",i_start
+        for i in xrange(i_start,2,-1):
             if debug: print "(i-1|i|i+1)",i,x[i],":",y[i-1],y[i],y[i+1]
             if y[i] < y[i-1]:
                 k += 1
